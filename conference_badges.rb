2319382587ec427baa326a14ad09a98d206acf2f
Francis Mendez <fmendez884@gmail.com>
2019-04-10 15:36:28 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+   i = 0
+  while ( i < array.length)
+    badges.push("#Hello, my name is #{array[i]}.")
+    i += 1
+  end
+end
+
+def assign_rooms
