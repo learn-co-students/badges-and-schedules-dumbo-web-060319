3f528a18e48a9e1170a1ded1896979af6b16c2ad
Alexander Schelchere <ascellie@gmail.com>
2019-05-11 23:13:17 -0400

    batch badge creator - complete

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,12 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-
+    badges = []
+    for i in attendees do
+        badge = "Hello, my name is #{i}."
+        badges.push(badge)
+    end
+    badges
 end
 
 def assign_rooms(attendees)
